90e9dfb740d9ccea400a78f9cbf2e52e53fde8ea
Kousik Das <kousikdas02@gmail.com>
2024-05-01 21:45:21 +0530

    feedback change

--- src/components/HomeMotelInfo/HomeMotelInfo.jsx
+++ src/components/HomeMotelInfo/HomeMotelInfo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { HomeMotelInfoWrapperStyled } from '../../styles/styledComponents/HomeMotelInfoWrapperStyled'
 import { Container, Typography, Box } from '@mui/material'
 import CustomButton from '../../ui/CustomButton/CustomButton'
+import { useNavigate } from 'react-router-dom'
 const HomeMotelInfo = () => {
+    const navigate = useNavigate();
   return (
     <HomeMotelInfoWrapperStyled>
         <Container fixed>
@@ -13,7 +15,7 @@ const HomeMotelInfo = () => {
                     </Typography>
                 </Box>
                 <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore autem quod rerum laboriosam. Aliquam ullam voluptatum alias? Recusandae sed dolore consequatur architecto praesentium assumenda illo cupiditate ipsum dolor corrupti. Cum!</Typography>
-                <CustomButton variant="black">LEARN MORE</CustomButton>
+                <CustomButton onClick={() => navigate('/your-stay')}>LEARN MORE</CustomButton>
             </Box>
         </Container>
     </HomeMotelInfoWrapperStyled>
